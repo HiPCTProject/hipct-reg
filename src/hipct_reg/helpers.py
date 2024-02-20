44e88712fd1a7d0c70dd36e13766dbfa3d46b011
David Stansby <dstansby@gmail.com>
2024-02-20 14:50:26 +0000

    Clean up helpers.py

--- src/hipct_reg/helpers.py
+++ src/hipct_reg/helpers.py
@@ -1,5 +1,5 @@
 import glob
-import sys
+from typing import Literal
 
 import dask_image
 import dask_image.imread
@@ -7,8 +7,6 @@ import numpy as np
 import SimpleITK as sitk
 import skimage.io
 import skimage.measure
-
-MAX_THREADS = 0  # 0 if all
 
 
 def import_im(
@@ -19,6 +17,8 @@ def import_im(
 ) -> sitk.Image:
     """
     Load a file into an ITK image.
+    - Reads all files into memory
+    - Casts data to float32
 
     Parameters
     ----------
@@ -32,37 +32,25 @@ def import_im(
         Downsample the image by a binning factor before returning.
 
     """
-    bin_factor = int(bin_factor)
-    pixelType = sitk.sitkInt16
-    pixelType = sitk.sitkFloat32
 
     file_type = test_file_type(path)
-    print(f"File type is .{file_type}")
-
     img_array_dask = dask_image.imread.imread(f"{path}/*.{file_type}")
-    print(img_array_dask)
 
     if crop_z is not None:
-        print(f"crop_z = {crop_z}")
-        img_array = img_array_dask[crop_z[0] : crop_z[1], :, :].compute()
-    else:
-        img_array = img_array_dask[:, :, :].compute()
+        img_array_dask = img_array_dask[crop_z[0] : crop_z[1], :, :]
 
-    print(img_array.shape)
+    img_array = img_array_dask.compute()
 
+    bin_factor = int(bin_factor)
     if bin_factor > 1:
-        print("\nBinning started\n")
-        print(f"Bin factor = {bin_factor}")
         # Binning
         img_array = skimage.measure.block_reduce(
             img_array, (bin_factor, bin_factor, bin_factor), np.mean
         )
 
-    print(img_array.shape)
-    print(img_array.dtype)
     image = sitk.GetImageFromArray(img_array)
     del img_array
-    image = sitk.Cast(image, pixelType)
+    image = sitk.Cast(image, sitk.sitkFloat32)
 
     image.SetOrigin([0, 0, 0])
     image.SetSpacing([pixel_size, pixel_size, pixel_size])
@@ -70,16 +58,15 @@ def import_im(
     return image
 
 
-def test_file_type(path):
+def test_file_type(path: str) -> Literal["tif", "jp2"]:
     N_tif = glob.glob(path + "/*.tif")
     N_jp2 = glob.glob(path + "/*.jp2")
 
     if len(N_tif) > 0 and len(N_jp2) > 0:
-        sys.exit("Error: tif and jp2 files in the folder")
+        raise RuntimeError("Error: tif and jp2 files in the folder")
     elif len(N_tif) == 0 and len(N_jp2) == 0:
-        sys.exit("Error: no tif or jp2 files in the folder")
+        raise RuntimeError("Error: no tif or jp2 files in the folder")
     elif len(N_tif) > 0 and len(N_jp2) == 0:
-        file_type = "tif"
-    elif len(N_jp2) > 0 and len(N_tif) == 0:
-        file_type = "jp2"
-    return file_type
+        return "tif"
+    else:
+        return "jp2"
