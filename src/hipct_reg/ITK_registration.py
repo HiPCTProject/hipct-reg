5f5e118c657c579e17b6cf2438f3efb0ede02458
David Stansby <dstansby@gmail.com>
2024-02-21 15:35:09 +0000

    Fix code coverage reporting

--- src/hipct_reg/ITK_registration.py
+++ src/hipct_reg/ITK_registration.py
@@ -70,7 +70,7 @@ def registration_rot(
     # Similarity metric settings.
     R.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
     R.SetMetricSamplingStrategy(R.NONE)
-    R.SetMetricSamplingPercentage(0.01)
+    R.SetMetricSamplingPercentage(0.01, seed=1)
 
     R.SetInterpolator(sitk.sitkLinear)
 
@@ -208,7 +208,7 @@ def registration_sitk(
     # Set registration metric settings
     R.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
     R.SetMetricSamplingStrategy(R.RANDOM)
-    R.SetMetricSamplingPercentage(0.1)
+    R.SetMetricSamplingPercentage(0.1, seed=1)
 
     # Set registration interpolator
     R.SetInterpolator(sitk.sitkLinear)
--- src/hipct_reg/tests/test_registration.py
+++ src/hipct_reg/tests/test_registration.py
@@ -1,5 +1,3 @@
-"""An example set of tests."""
-
 import logging
 from pathlib import Path
 from typing import Any
