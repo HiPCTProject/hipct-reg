f3f3c18b3a423f15f621c44c6b7a955daa373db6
David Stansby <dstansby@gmail.com>
2024-03-18 16:13:22 +0000

    Use thin slab in z for registration

--- src/hipct_reg/data.py
+++ src/hipct_reg/data.py
@@ -22,9 +22,9 @@ datasets = {d.name: d for d in load_datasets()}
 
 
 @dataclass
-class Cube:
+class Cuboid:
     """
-    Represents a small cube of data located within a full dataset.
+    Represents a small cuboid of data located within a full dataset.
 
     Attributes
     ----------
@@ -32,13 +32,16 @@ class Cube:
         Dataset.
     centre_point :
         Index of the centre of the cube.
-    size :
-        Half the size of an edge of the cube.
+    size_xy :
+        Half the size of an edge of cuboid in the x-y plane.
+    size_z :
+        Half the thickness of the cuboid along the z-axis.
     """
 
     ds: HiPCTDataSet
     centre_point: tuple[int, int, int]
-    size: int
+    size_xy: int
+    size_z: int
 
     @property
     def local_zarr_path(self) -> Path:
@@ -47,7 +50,7 @@ class Cube:
         """
         return (
             STORAGE_DIR
-            / f"{self.ds.name}_{'_'.join([str(i) for i in self.centre_point])}_{self.size}.zarr"
+            / f"{self.ds.name}_{'_'.join([str(i) for i in self.centre_point])}_{self.size_xy}_{self.size_z}.zarr"
         )
 
     def get_image(self) -> sitk.Image:
@@ -55,7 +58,11 @@ class Cube:
         Get cube as a SimpleITK image.
         """
         spacing = self.ds.resolution_um
-        origin = [(cp - self.size) * spacing for cp in self.centre_point]
+        origin = [
+            (self.centre_point[0] - self.size_xy) * spacing,
+            (self.centre_point[1] - self.size_xy) * spacing,
+            (self.centre_point[2] - self.size_z) * spacing,
+        ]
 
         image = sitk.GetImageFromArray(self.get_array().T)
         image.SetSpacing((spacing, spacing, spacing))
@@ -79,9 +86,11 @@ class Cube:
         gcs_store = self.get_gcs_store()
         data = (
             gcs_store[
-                self.centre_point[0] - self.size : self.centre_point[0] + self.size,
-                self.centre_point[1] - self.size : self.centre_point[1] + self.size,
-                self.centre_point[2] - self.size : self.centre_point[2] + self.size,
+                self.centre_point[0] - self.size_xy : self.centre_point[0]
+                + self.size_xy,
+                self.centre_point[1] - self.size_xy : self.centre_point[1]
+                + self.size_xy,
+                self.centre_point[2] - self.size_z : self.centre_point[2] + self.size_z,
             ]
             .read()
             .result()
@@ -106,12 +115,12 @@ def get_reg_input(
     full_name: str,
     roi_point: tuple[int, int, int],
     full_point: tuple[int, int, int],
-    full_size: int = 64,
+    full_size_xy: int = 64,
 ) -> RegistrationInput:
     """
     Given the dataset of a ROI scan, get:
-    - a 128 x 128 x 128 cube of it's parent full-organ scan
-    - the equivalent (larger) cube of the ROI itself
+    - a (full_size_xy x 2) x (full_size_xy x 2) x 32 cubiod of it's parent full-organ scan
+    - the equivalent (larger) cube of the ROI itself.
 
     The size of the full-organ scan cube can be changed.
 
@@ -121,16 +130,24 @@ def get_reg_input(
     assert (
         full_dataset.is_complete_organ
     ), "Full dataset name given is not a full organ dataset"
-    full_cube = Cube(full_dataset, full_point, size=full_size)
+    full_size_z = 16
+    full_cube = Cuboid(
+        full_dataset, full_point, size_xy=full_size_xy, size_z=full_size_z
+    )
 
     roi_dataset = datasets[roi_name]
     assert not roi_dataset.is_complete_organ, "ROI dataset name given is a ROI dataset"
-    roi_size = int(full_size * full_dataset.resolution_um / roi_dataset.resolution_um)
-    roi_cube = Cube(roi_dataset, roi_point, size=roi_size)
+    roi_size_xy = int(
+        full_size_xy * full_dataset.resolution_um / roi_dataset.resolution_um
+    )
+    roi_size_z = int(
+        full_size_z * full_dataset.resolution_um / roi_dataset.resolution_um
+    )
+    roi_cube = Cuboid(roi_dataset, roi_point, size_xy=roi_size_xy, size_z=roi_size_z)
 
     return RegistrationInput(
         roi_image=roi_cube.get_image(),
         full_image=full_cube.get_image(),
-        common_point_full=(full_size, full_size, full_size),
-        common_point_roi=(roi_size, roi_size, roi_size),
+        common_point_full=(full_size_xy, full_size_xy, full_size_z),
+        common_point_roi=(roi_size_xy, roi_size_xy, roi_size_z),
     )
