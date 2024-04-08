eb903010484b9cee4c8066ab110d80968bd2063f
David Stansby <dstansby@gmail.com>
2024-04-08 13:45:29 +0100

    add basic doc layout

---
+++ docs/tutorial/tutorial.py
@@ -0,0 +1,14 @@
+"""
+Tutorial
+========
+
+This page steps through running the HiP-CT registration pipeline.
+"""
+
+
+# %%
+# Example heading
+# ---------------
+#
+# We'll start by generating a set of sample data to downsample.
+# To do this we'll just save 35 copies of a grayscale cat to a temporary directory.
