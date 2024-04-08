3f0f41289f530ccadd6fd72452a20f04e3196d2d
David Stansby <dstansby@gmail.com>
2024-04-08 13:52:16 +0100

    Don't run sphinx gallery examples

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,6 @@ intersphinx_mapping = {
 sphinx_gallery_conf = {
     "examples_dirs": "tutorial",  # path to your example scripts
     "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
-    "filename_pattern": "/",
 }
 
 # -- Options for HTML output -------------------------------------------------
