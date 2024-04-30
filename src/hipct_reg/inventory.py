5ad4ef349c7e0bcbba1e35283c86988c9961f962
David Stansby <dstansby@gmail.com>
2024-04-30 17:30:42 +0100

    Add registration method

--- src/hipct_reg/inventory.py
+++ src/hipct_reg/inventory.py
@@ -2,7 +2,11 @@ from pathlib import Path
 from typing import Any
 
 import pandas as pd
+import SimpleITK as sitk
 from pydantic import BaseModel
+
+from hipct_reg.data import get_reg_input
+from hipct_reg.registration import run_registration
 
 INVENTORY_FILE = Path(__file__).parent / "reg_inventory.csv"
 
@@ -26,7 +30,7 @@ class Dataset(BaseModel):
     common_point_parent_z: int | None = None
 
     # Free text notes, mainly to note bad data quality
-    notes: str = ""
+    notes: str | None = None
 
     # Registration parameters
     tx: float | None = None
@@ -34,6 +38,47 @@ class Dataset(BaseModel):
     tz: float | None = None
     rotation: float | None = None
     scale: float | None = None
+    reg_metric: float | None = None
+
+    def register(self) -> None:
+        assert (
+            self.common_point_x is not None
+            and self.common_point_y is not None
+            and self.common_point_z is not None
+        ), "Common point values not set"
+        assert (
+            self.common_point_parent_x is not None
+            and self.common_point_parent_y is not None
+            and self.common_point_parent_z is not None
+        ), "Common point parent values not set"
+
+        reg_input = get_reg_input(
+            roi_name=self.name,
+            roi_point=(self.common_point_x, self.common_point_y, self.common_point_z),
+            full_name=self.parent_name,
+            full_point=(
+                self.common_point_parent_x,
+                self.common_point_parent_y,
+                self.common_point_parent_z,
+            ),
+            full_size_xy=64,
+        )
+        transform, metric = run_registration(reg_input)
+
+        # Convert so transform is from (0, 0, 0) instead of around the rotation point
+        translation = transform.TransformPoint((0, 0, 0))
+        new_transform = sitk.Similarity3DTransform()
+        new_transform.SetParameters(transform.GetParameters())
+        new_transform.SetTranslation(translation)
+
+        translation = new_transform.GetTranslation()
+        self.tx = translation[0]
+        self.ty = translation[1]
+        self.tz = translation[2]
+        self.scale = new_transform.GetScale()
+        self.rotation = new_transform.GetVersor()[0]
+
+        self.reg_metric = metric
 
 
 def save_datasets(datasets: list[Dataset]) -> None:
